9df96a7a35acdad6149f495598cd3c0790787948
Cuongvx <cuongvx@mobio.io>
2022-10-11 21:51:53 +0700

    Init project

---
+++ PermissionLib/PermissionLib.h
@@ -0,0 +1,18 @@
+//
+//  PermissionLib.h
+//  PermissionLib
+//
+//  Created by sun on 11/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PermissionLib.
+FOUNDATION_EXPORT double PermissionLibVersionNumber;
+
+//! Project version string for PermissionLib.
+FOUNDATION_EXPORT const unsigned char PermissionLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PermissionLib/PublicHeader.h>
+
+
